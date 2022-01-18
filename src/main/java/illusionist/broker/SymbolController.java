64b01efd64b43249164a035e5d0ca718459c89e9
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2022-01-18 19:54:52 +0545

    creating Query variables and added unit test case

--- src/main/java/illusionist/broker/SymbolController.java
+++ src/main/java/illusionist/broker/SymbolController.java
@@ -3,9 +3,12 @@ package illusionist.broker;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.QueryValue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller("/symbols")
 public class SymbolController {
@@ -24,5 +27,14 @@ public class SymbolController {
   @Get("{value}")
   public Symbol getSymbolByValue(@PathVariable String value){
     return memoryStore.getSymbols().get(value);
+  }
+
+  @Get("/filter{?max,offset}")
+  public List<Symbol> getSymbols(@QueryValue Optional<Integer> max, @QueryValue Optional<Integer> offset){
+    return memoryStore.getSymbols().values()
+      .stream()
+      .skip(offset.orElse(0))
+      .limit(max.orElse(0))
+      .collect(Collectors.toList());
   }
 }
--- src/test/java/illusionist/broker/SymbolControllerTest.java
+++ src/test/java/illusionist/broker/SymbolControllerTest.java
@@ -4,18 +4,19 @@ import com.fasterxml.jackson.databind.JsonNode;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
-import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
-
-import jakarta.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class SymbolControllerTest {
+
+  private static final Logger LOG = LoggerFactory.getLogger(SymbolControllerTest.class);
 
   @Inject
   @Client("/symbols")
@@ -30,19 +31,39 @@ class SymbolControllerTest {
   }
 
   @Test
-  void symbolsEmdpointReturnsListOfSymbol() {
+  void symbolsEndpointReturnsListOfSymbol() {
     var response = client.toBlocking().exchange("/", JsonNode.class);
     assertEquals(HttpStatus.OK, response.getStatus());
     assertEquals(10, response.getBody().get().size());
   }
 
   @Test
-  void symbolsEmdpointReturnsTheCorrectSymbol() {
+  void symbolsEndpointReturnsTheCorrectSymbol() {
     var testSymbol = new Symbol("TEST");
+    LOG.debug("testSymbol: {}", testSymbol.getValue());
     inMemoryStore.getSymbols().put(testSymbol.getValue(), testSymbol);
-    var response = client.toBlocking().exchange("/"+testSymbol.getValue(), Symbol.class);
+    LOG.debug("testSymbol: {}", testSymbol);
+    var response = client.toBlocking().exchange("/" + testSymbol.getValue(), Symbol.class);
     assertEquals(HttpStatus.OK, response.getStatus());
     assertEquals(testSymbol, response.getBody().get());
+  }
+
+  @Test
+  void symbolsEndpointReturnsListOfSymbolTakingQueryParametersIntoAccount() {
+    var max10 = client.toBlocking().exchange("/filter?max=10", JsonNode.class);
+    assertEquals(HttpStatus.OK, max10.getStatus());
+    LOG.debug("Max: 10: {}", max10.getBody().get().toPrettyString());
+    assertEquals(10, max10.getBody().get().size());
+
+    var offset7  = client.toBlocking().exchange("/filter?offset=7", JsonNode.class);
+    assertEquals(HttpStatus.OK, offset7.getStatus());
+    LOG.debug("Offset: 7: {}", offset7.getBody().get().toPrettyString());
+    assertEquals(3, offset7.getBody().get().size());
+
+    var max2Offset7  = client.toBlocking().exchange("/filter?max=2&offset=7", JsonNode.class);
+    assertEquals(HttpStatus.OK, max2Offset7.getStatus());
+    LOG.debug("Max: 2: Offset: 7: {}", max2Offset7.getBody().get().toPrettyString());
+    assertEquals(2, max2Offset7.getBody().get().size());
   }
 
 }
